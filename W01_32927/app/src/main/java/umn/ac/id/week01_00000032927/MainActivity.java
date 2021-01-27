9bcf864294225ebac3a527bd550c2267535f02b2
Kevin Andreejan Susanto <kevin.andreejan@student.umn.ac.id>
2021-01-27 15:32:45 +0700

    First Commit Adding Week1

---
+++ W01_32927/app/src/main/java/umn/ac/id/week01_00000032927/MainActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.week01_00000032927;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
